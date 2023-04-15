13b0ac1f90b3ff7e5702781e86fd151131283ef4
vibhanshujainiiitr <cs19b1027@iiitr.ac.in>
2023-04-15 15:06:16 +0530

    Merged the Indic Trans

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
+import json
+import pickle
+from typing import *
+import numpy as np
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import Response
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from fastapi import HTTPException
+
+# TTS
 from TTS.utils.synthesizer import Synthesizer
 
+from pydantic import BaseModel
+
+# IndicTrans
+from src.indicTrans.inference.engine import IndicTransModel
+
+# Indic TTS
 from src.tts.inference import TextToSpeechEngine
 from src.tts.models.request import TTSRequest
 
-SUPPORTED_LANGUAGES = {
+# En-Indic
+en2indic_model = IndicTransModel(expdir="models/en-indic")
+
+TTS_SUPPORTED_LANGUAGES = {
     'as' : "Assamese - অসমীয়া",
     'bn' : "Bangla - বাংলা",
     'brx': "Boro - बड़ो",
@@ -25,8 +43,10 @@ SUPPORTED_LANGUAGES = {
     'te' : "Telugu - తెలుగు",
 }
 
+INDIC_TTS_SUPPORTED_LANGUAGES = ["as", "hi", "mr", "ta", "bn", "kn", "or", "te", "gu", "ml", "pa"]
+
 models = {}
-for lang in SUPPORTED_LANGUAGES:
+for lang in TTS_SUPPORTED_LANGUAGES:
     models[lang]  = Synthesizer(
         tts_checkpoint=f'models/tts/{lang}/fastpitch/best_model.pth',
         tts_config_path=f'models/tts/{lang}/fastpitch/config.json',
@@ -54,17 +74,88 @@ api.add_middleware(
     allow_headers=["*"],
 )
 
-@api.get("/supported_languages")
-def get_supported_languages():
-    return SUPPORTED_LANGUAGES
-
 @api.get("/")
 def homepage():
-    return "AI4Bharat Text-To-Speech API"
+    return "Y4J System is live"
+
+@api.get("/supported_languages")
+def get_supported_languages():
+    return TTS_SUPPORTED_LANGUAGES
+
 
 @api.post("/")
 async def batch_tts(request: TTSRequest, response: Response):
     return engine.infer_from_request(request)
+
+# Indic Trans
+
+class requestSchemaTargetTranslate(BaseModel):
+    data: str
+    targetLan: str
+    currLan: str
+
+
+class requestSchemaAllTranslate(BaseModel):
+    data: str
+
+
+# Target Language Translation
+@api.get("/targetTranslate")
+async def targetTranslate(request: requestSchemaTargetTranslate):
+    request = request.dict()
+
+    data = request["data"]
+    data = [data]
+    currLan = request["currLan"]
+    targetLan = request["targetLan"]
+    translationResult = en2indic_model.batch_translate(data, currLan, targetLan)
+
+    result = {
+        "success": True,
+        "message": "Target Translation from " + currLan + " to " + targetLan,
+        "data": translationResult,
+    }
+    # Return the response
+    return result
+
+
+# All Language Translation at once
+@api.get("/allTranslate")
+async def allTranslate(request: requestSchemaAllTranslate):
+    request = request.dict()
+
+    # Get the data from the request
+    data = request["data"]
+    data = [data]
+    currLan = "en"
+
+    res = {
+        "as": "",
+        "hi": "",
+        "mr": "",
+        "ta": "",
+        "bn": "",
+        "kn": "",
+        "or": "",
+        "te": "",
+        "gu": "",
+        "ml": "",
+        "pa": "",
+    }
+
+    # Translate the data in all the target languages
+    for lang in INDIC_TTS_SUPPORTED_LANGUAGES:
+        result = en2indic_model.batch_translate(data, currLan, lang)
+        res[lang] = result[0]
+
+    # Standard return format
+    result = {
+        "success": True,
+        "message": "All Translation from " + currLan + " to " + str(INDIC_TTS_SUPPORTED_LANGUAGES),
+        "data": res,
+    }
+    # Return the response
+    return result
 
 if __name__ == "__main__":
     # uvicorn main:api --host 0.0.0.0 --port 5050 --log-level info
